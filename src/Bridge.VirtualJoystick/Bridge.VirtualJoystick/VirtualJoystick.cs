a62247f5a8c97b110de83a6b44f61cba4c16c780
Unknown <ethan@fasetto.com>
2017-11-13 14:02:29 -0600

    updated comments and added Emit and HasListeners

--- src/Bridge.VirtualJoystick/Bridge.VirtualJoystick/VirtualJoystick.cs
+++ src/Bridge.VirtualJoystick/Bridge.VirtualJoystick/VirtualJoystick.cs
@@ -27,8 +27,8 @@ namespace Bridge.VirtualJoystick
         /// <summary>
         /// Listen on the given event with the given listener.
         /// </summary>
-        /// <param name="eventName">The name of the event</param>
-        /// <param name="eventListener">The event listener</param>
+        /// <param name="eventName">The name of the event.</param>
+        /// <param name="eventListener">The event listener.</param>
         /// <returns>The joystick.</returns>
         public VirtualJoystick AddEventListener(string eventName, Delegate eventListener)
         {
@@ -36,18 +36,18 @@ namespace Bridge.VirtualJoystick
         }
 
         /// <summary>
-        /// Gets the delta X coordinate of the joystick
+        /// Gets the delta X coordinate of the joystick.
         /// </summary>
-        /// <returns>The delta X coordinate</returns>
+        /// <returns>The delta X coordinate.</returns>
         public int DeltaX()
         {
             return default(int);
         }
 
         /// <summary>
-        /// Gets the delta Y coordinate of the joystick
+        /// Gets the delta Y coordinate of the joystick.
         /// </summary>
-        /// <returns>The delta Y coordinate</returns>
+        /// <returns>The delta Y coordinate.</returns>
         public int DeltaY()
         {
             return default(int);
@@ -57,6 +57,27 @@ namespace Bridge.VirtualJoystick
         /// Clean up the joystick.
         /// </summary>
         public void Destroy() {}
+
+        /// <summary>
+        /// Emits the specified event.
+        /// </summary>
+        /// <param name="eventName">The name of the event.</param>
+        /// <param name="arguments">The event's arguments.</param>
+        /// <returns></returns>
+        public VirtualJoystick Emit(string eventName, params object[] arguments)
+        {
+            return default(VirtualJoystick);
+        }
+
+        /// <summary>
+        /// Check if the joystick has any event listeners.
+        /// </summary>
+        /// <param name="eventName">The name of the event.</param>
+        /// <returns>true if the event has listeners, otherwise false.</returns>
+        public bool HasListeners(string eventName)
+        {
+            return default(bool);
+        }
 
         #endregion
     }
